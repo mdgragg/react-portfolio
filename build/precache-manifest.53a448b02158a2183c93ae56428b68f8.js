598b85730e4288827331ffff3ba615d7d149dd05
Michael Gragg <contact@michaeldgragg.com>
2020-10-17 11:13:58 -0400

    added react code

---
+++ build/precache-manifest.53a448b02158a2183c93ae56428b68f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844c25ddf46e8d426d0096406016d2ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630bbae8c92845c7cda1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88653eab6e647e69461c",
+    "url": "/static/js/2.e2309a10.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.e2309a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630bbae8c92845c7cda1",
+    "url": "/static/js/main.84e7121f.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
