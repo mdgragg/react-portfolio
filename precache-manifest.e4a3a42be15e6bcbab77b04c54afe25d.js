105ee0098b6faaebc4290c0ac7bc945b48e1ea33
Michael Gragg <contact@michaeldgragg.com>
2020-11-05 10:52:52 -0500

    Updates

--- precache-manifest.e4a3a42be15e6bcbab77b04c54afe25d.js
+++ precache-manifest.e4a3a42be15e6bcbab77b04c54afe25d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06c0681c655aacf369cb8ce4dae4a73c",
+    "revision": "17c00b2900e10a0a585f77d9da796f07",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.3cf5d70f.chunk.css"
   },
   {
-    "revision": "bc55495c16471e419be3",
+    "revision": "50cd9554c5c5117cd615",
     "url": "/react-portfolio/static/css/main.6d519d29.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.c2e4fbfc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bc55495c16471e419be3",
-    "url": "/react-portfolio/static/js/main.dfb2cc1a.chunk.js"
+    "revision": "50cd9554c5c5117cd615",
+    "url": "/react-portfolio/static/js/main.b0098bc2.chunk.js"
   },
   {
     "revision": "26950ce1f34a1b315732",
