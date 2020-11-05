2060ee108c951a386845b9591a81d79a3e616709
Michael Gragg <contact@michaeldgragg.com>
2020-11-05 09:33:27 -0500

    Updates

---
+++ precache-manifest.009951ded4328e9b2bd9e5589fe5045f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845bc96e8f5623d6f1e8a649182d2df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52469cdf1da4ebacb923",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "6010e6bc17126c438f67",
+    "url": "/static/css/main.6d519d29.chunk.css"
+  },
+  {
+    "revision": "52469cdf1da4ebacb923",
+    "url": "/static/js/2.c2e4fbfc.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.c2e4fbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6010e6bc17126c438f67",
+    "url": "/static/js/main.f6dcf418.chunk.js"
+  },
+  {
+    "revision": "89282a425a06e45fd0f3",
+    "url": "/static/js/runtime-main.c4d7dc80.js"
+  }
+]);
