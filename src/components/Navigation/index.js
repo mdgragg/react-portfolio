2fc10c7374a2afcfbdc6b866eac77bd2a7b1cce2
Michael Gragg <contact@michaeldgragg.com>
2020-12-01 20:39:24 -0500

    fixed responsive nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,29 +6,34 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 function Navigation() {
   return (
     <React.Fragment >
-      <Navbar className="nav fixed-top"> 
-      <Container>
-        <Navbar.Brand as={Link} to="/react-portfolio" className="navbar-brand text-white" href="home page">
-          MICHAEL GRAGG
-        </Navbar.Brand>
-          <Nav className="navbar-nav justify-content-end">
 
-            <Nav.Link as={Link} to="/react-portfolio" className="nav-item text-white-50 about-link" href="home page">
-              About
-            </Nav.Link>
 
-            <Nav.Link as={Link} to="/portfolio" className="nav-item text-white-50 portfolio-link" href="portfolio page">
-              Portfolio
-            </Nav.Link>
+<Navbar className="nav fixed-top" collapseOnSelect expand="lg"  variant="dark">
+<Container>
+  <Navbar.Brand as={Link} to="/react-portfolio" className="navbar-brand text-white" href="home page">
+    MICHAEL GRAGG
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto"></Nav>
+    <Nav>
+        <Nav.Link as={Link} to="/react-portfolio" className="nav-item " href="home page">
+          About
+        </Nav.Link>
 
-            <a className="nav-link nav-item text-white-50" href="react-portfolio/Gragg-Resume.pdf" target="_blank" rel="noopener noreferrer">
-              Resume
-            </a>
+        <Nav.Link as={Link} to="/portfolio" className="nav-item" href="portfolio page">
+          Portfolio
+        </Nav.Link>
 
-          </Nav>
-          </Container>
-      </Navbar>
-    </React.Fragment>
+        <a className="nav-link nav-item" href="react-portfolio/Gragg-Resume.pdf" target="_blank" rel="noopener noreferrer">
+          Resume
+        </a>
+
+    </Nav>
+  </Navbar.Collapse>
+</Container>
+</Navbar>
+</React.Fragment>
   );
 }
 
