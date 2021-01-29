b6a7733b40c8a83f357041e83d384fc72d7a2213
Michael Gragg <contact@michaeldgragg.com>
2021-01-29 10:30:52 -0500

    added styles to skills page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./style.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5 } from '@fortawesome/free-regular-svg-icons'
+import { faHtml5, faCss3Alt, faJs, faBootstrap, faReact, faNode} from '@fortawesome/free-brands-svg-icons'
+
 import HeaderSkills from "../HeaderSkills";
 
 
@@ -16,43 +17,51 @@ function Skills() {
             <Col md={12}>
               <Row className="skills-row">
               <Col md={3}>
-                Photoshop
+                <img class="skills-icon" src="/assets/icons/skills/photoshop.png" alt="photoshop logo" />
+                Adobe Photoshop
               </Col>
               <Col md={3}>
-                Illustrator
+                <img class="skills-icon" src="/assets/icons/skills/illustrator.png" alt="illustrator logo" />
+                Adobe Illustrator
               </Col>
               <Col md={3}>
-                InDesign
+                <img class="skills-icon" src="/assets/icons/skills/indesign.png" alt="indesign logo" /> 
+                Adobe InDesign
               </Col>
               <Col md={3}>
-
-<FontAwesomeIcon icon={Coffee} />
-<FontAwesomeIcon icon={faHtml5} />
-<i class="fab fa-html5"></i>
+                <FontAwesomeIcon icon={faHtml5} />
                 HTML5
               </Col>
               <Col md={3}>
+                <FontAwesomeIcon icon={faCss3Alt} />
                 CSS
               </Col>
               <Col md={3}>
+                <FontAwesomeIcon icon={faJs} />
                 JavaScript
               </Col>
               <Col md={3}>
+                <FontAwesomeIcon icon={faBootstrap} />
                 Bootstrap
               </Col>
               <Col md={3}>
+                <i class="icon-jquery"></i>
                 jQuery
               </Col>
               <Col md={3}>
+                <FontAwesomeIcon icon={faReact} />
                 React.js
               </Col>
               <Col md={3}>
+                <i class="icon-mongodb"></i>
                 MongoDB
               </Col>
               <Col md={3}>
+                <i class="icon-mysql-alt"></i>
                 MySQL
               </Col>
               <Col md={3}>
+                <FontAwesomeIcon icon={faNode} />
                 Node.js
               </Col>
               </Row>
